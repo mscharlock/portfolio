fd3f8993a7fc9501aacd44cee37416f323681fa3
Michelle Scharlock <michelle.scharlock@gmail.com>
2017-07-12 12:09:59 -0700

    Working on making projects show up

--- js/app.js
+++ js/app.js
@@ -1,37 +1,41 @@
 'use-strict';
-$(document).ready(function(){
-  var projects = [];
+var projects = [];
 
-  function Project(rawProject) {
-    this.title = rawProject.title;
-    this.description = rawProject.description;
-    this.date = rawProject.date;
-    this.inClass = rawProject.inClass;
-    this.langs = rawProject.langs;
-  }
+function Project(rawProject) {
+  this.title = rawProject.title;
+  this.description = rawProject.description;
+  this.date = rawProject.date;
+  this.inClass = rawProject.inClass;
+  this.langs = rawProject.langs;
+}
 
-  Project.prototype.toHtml = function() {
-    var $newProject = $('projects.template').clone();
-    $newProject.data('title', this.title);
-    $newProject.data('description', this.description);
-    $newProject.data('date', this.date);
-    $newProject.data('inClass', this.inClass);
-    $newProject.data('langs', this.langs);
-    $newProject.append('<hr>');
+Project.prototype.toHtml = function() {
+  var $newProject = $('project.template').clone();
+  $newProject.removeclass('template');
 
-    return $newProject;
-  }
+  $newProject.find('h3').text(this.title);
+  // $newProject.data('description', this.description);
+  // $newProject.data('date', this.date);
+  // $newProject.data('inClass', this.inClass);
+  // $newProject.data('langs', this.langs);
+  // $newProject.append('<hr>');
 
-  listofProjects.forEach(function(projectObj) {
-    projects.push(new Project(projectObj));
-  })
+  return $newProject;
+}
 
-  listofProjects.forEach(function(project) {
-    $('#projects').append(project.toHtml());
-  })
+// rawData.forEach(function(articleObject) {
+//   // REVIEW: Take a look at this forEach method; This may be the first time we've seen it.
+//   articles.push(new Article(articleObject));
+// })
+//
+// articles.forEach(function(article) {
+//   $('#articles').append(article.toHtml());
+// })
+
+listOfProjects.forEach(function(projectObj) {
+  projects.push(new Project(projectObj));
 })
 
-
-  // FROM LAB:
-  // articles.forEach(function(article) {
-  // $('#articles').append(article.toHtml());
+listOfProjects.forEach(function(project) {
+  $('#projects').append(project.toHtml());
+})
