9a3a90822d277417b841e917ebffd71554c1eba0
Michelle Scharlock <michelle.scharlock@gmail.com>
2017-07-13 10:33:28 -0700

    Working on getting nav to show/hide properly

--- js/hide-show.js
+++ js/hide-show.js
@@ -6,18 +6,26 @@ var viewPage = {};
 
 //Some methods to hide/show//
 viewPage.hideSections = function() {
-  $('.hideData').hide();
+  $('section[data-hide="hideData"]').hide();
+  $('nav li').hide();
 }
 
-viewPage.hideNav = function() {
+viewPage.openNav = function() {
   $('nav').on('click', function() {
     $('nav li').show();
+    $('#' + $(this).attr('data-goTo')).show();
+    $('nav li').hide();
+  })
+}
+
+    // $('nav li').show();
     // $('nav li:nth-child(1)').find('#'). //something here about selecting the right li and then replacing the # with the right class? Or maybe I need to rethink that bit
     //and then chain to the end of that...
     // .show()
 
-  })
-}
+  //   console.log($(this).attr('data-content'));
+  //  $('.tab-content').hide();
+  //  $('#' + $(this).attr('data-content')).show();
 
 //Pseudo-code the above problem:
 //When we run viewPage.hideNav, on a click, we want the lis to appear. We also want each li to be linked (href) to their respective hidden part page (which I've designated as a class but maybe I should rethink that) and then have that page actually show
@@ -25,5 +33,6 @@ viewPage.hideNav = function() {
 
 $(document).ready(function(){
   viewPage.hideSections();
-  viewPage.hideNav();
+  viewPage.openNav();
+  // viewPage.selectLink();
 })
