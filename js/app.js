f46b9cea544b339e618eb555483ba49da8d5b7d0
Michelle Scharlock <michelle.scharlock@gmail.com>
2017-07-12 22:29:03 -0700

    Portfolio projects are appearing!

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-'use-strict';
+'use strict';
 var projects = [];
 
 function Project(rawProject) {
@@ -10,32 +10,23 @@ function Project(rawProject) {
 }
 
 Project.prototype.toHtml = function() {
-  var $newProject = $('project.template').clone();
-  $newProject.removeclass('template');
-
+  var $newProject = $('article.template').clone();
+  $newProject.removeClass('template');
+  //
+  // //where we'll match all the things//
   $newProject.find('h3').text(this.title);
-  // $newProject.data('description', this.description);
-  // $newProject.data('date', this.date);
-  // $newProject.data('inClass', this.inClass);
-  // $newProject.data('langs', this.langs);
-  // $newProject.append('<hr>');
-
+  $newProject.find('.description').html(this.description);
+  $newProject.find('.date').html('Created on ' + this.date);
+  $newProject.find('.inClass').html(this.inClass);
+  $newProject.find('.langs').html(this.langs);
+  $newProject.append('<hr>');
   return $newProject;
-}
+};
 
-// rawData.forEach(function(articleObject) {
-//   // REVIEW: Take a look at this forEach method; This may be the first time we've seen it.
-//   articles.push(new Article(articleObject));
-// })
-//
-// articles.forEach(function(article) {
-//   $('#articles').append(article.toHtml());
-// })
+listOfProjects.forEach(function(projectObject){
+  projects.push(new Project(projectObject));
+});
 
-listOfProjects.forEach(function(projectObj) {
-  projects.push(new Project(projectObj));
-})
-
-listOfProjects.forEach(function(project) {
+projects.forEach(function(project) {
   $('#projects').append(project.toHtml());
 })
