6ce96a6e705b769567e7f771907bb3fa7ff85ed3
Michelle Scharlock <michelle.scharlock@gmail.com>
2017-07-19 09:53:50 -0700

    IT'S WORKING! HUZZAH!

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static('/public'));
 
-app.post('/js/ //not sure if this right//', bodyParser, function(request, response) {
-  console.log(request.body);
-  response.send('Helloooo the server is listening to you!!');
-});
+// app.post('/notsurewhatgoeshere', bodyParser, function(request, response) {
+//   console.log(request.body);
+//   response.send('Helloooo the server is listening to you!!');
+// });
 
 app.listen(PORT, function(){
   console.log(`'listening on port number: ${PORT}'`);
