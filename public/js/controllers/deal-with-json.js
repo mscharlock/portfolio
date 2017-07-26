fdb4be9c8221ae277993d1e8bc4318671c7d2120
Michelle Scharlock <michelle.scharlock@gmail.com>
2017-07-26 11:13:27 -0700

    Added routes.js and moved files around

---
+++ public/js/controllers/deal-with-json.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const successfulCallback = function(data) {
+  localStorage.setItem('rawProjects', JSON.stringify(data));
+}
+
+const errorCallback = function(err) {
+  console.error(err);
+}
+
+if (localStorage.rawProjects) {
+  JSON.parse(localStorage.rawProjects)
+} else {
+  $.getJSON('projectsJson.json') //for some reason this isn't working??//
+    .then(successfulCallback, errorCallback);
+}
---
+++ public/js/controllers/routes.js
@@ -0,0 +1,15 @@
+'use strict'
+var app = app || {}; //if I use this, then I have to figure out what is attached to app//
+
+//TODO:I gotta change this so the functionName handles the specific hide/show of the page//
+page('/', functionName);
+page('/portfolio', functionName);
+page('/resume', functionName);
+page('/accomplishments', functionName);
+page('/languages', functionName);
+page('/volunteering', functionName);
+page('contact', functionName);
+page('/skills', functionName);
+page('/about', functionName);
+
+page();
